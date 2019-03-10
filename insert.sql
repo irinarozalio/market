77a044fb53a1bdf60ef6cfa3d9e83f6acec99534
Irina Rozalio <irinaro@cellcom.co.il>
2019-03-10 17:36:35 +0200

    New Files

--- insert.sql
+++ insert.sql
@@ -1,71 +1,78 @@
+TRUNCATE stocks CASCADE;
+delete from stocks;
+delete from products;
+delete from colors;
+delete from fabric;
+delete from locations;
+delete from product_size;
 --colors
-INSERT INTO market.colors VALUES
+INSERT INTO colors VALUES
     (1, 'Multicolor');
-INSERT INTO market.colors VALUES
+INSERT INTO colors VALUES
     (2, 'Black');
-INSERT INTO market.colors VALUES
+INSERT INTO colors VALUES
     (3, 'Yellow');
-INSERT INTO market.colors VALUES
+INSERT INTO colors VALUES
     (4, 'Khaki');
-INSERT INTO market.colors VALUES
+INSERT INTO colors VALUES
     (5, 'Blue');
 
 --fabric
-INSERT INTO market.fabric VALUES
+INSERT INTO fabric VALUES
     (1, 'Polyester');
-INSERT INTO market.fabric VALUES
+INSERT INTO fabric VALUES
     (2, 'Leather');
-INSERT INTO market.fabric VALUES
+INSERT INTO fabric VALUES
     (3, 'Cotton');
-INSERT INTO market.fabric VALUES
+INSERT INTO fabric VALUES
     (4, 'Silk');
-INSERT INTO market.fabric VALUES
+INSERT INTO fabric VALUES
     (5, 'Tencel');
 
 --locations
-INSERT INTO market.locations VALUES
+INSERT INTO locations VALUES
     (1, 'Los Angeles');
-INSERT INTO market.locations VALUES
+INSERT INTO locations VALUES
     (2, 'Tel Aviv');
-INSERT INTO market.locations VALUES
+INSERT INTO locations VALUES
     (3, 'Kiev');
-INSERT INTO market.locations VALUES
+INSERT INTO locations VALUES
     (4, 'Moskva');
-INSERT INTO market.locations VALUES
+INSERT INTO locations VALUES
     (5, 'New York');
 
 --product_size
-INSERT INTO market.product_size VALUES
+INSERT INTO product_size VALUES
     (1, 'Medium');
-INSERT INTO market.product_size VALUES
+INSERT INTO product_size VALUES
     (2, 'Small');
-INSERT INTO market.product_size VALUES
+INSERT INTO product_size VALUES
     (3, 'Large');
-INSERT INTO market.product_size VALUES
+INSERT INTO product_size VALUES
     (4, 'XLarge');
-INSERT INTO market.product_size VALUES
+INSERT INTO product_size VALUES
     (5, 'XSmall');
 
 --products
-INSERT INTO market.products VALUES
-    (1, 'Shoulder Knot Leopard Print Dress',1,1,1);
-INSERT INTO market.products VALUES
-    (2, 'Buckle Belted Rolled Hem PU Shorts',2,2,2);
-INSERT INTO market.products VALUES
-    (3, 'Ruffle Trim Striped Top And High Split Side Pants Set',3,3,3);
-INSERT INTO market.products VALUES
-    (4, 'Crisscross Open Back Dot Strappy Romper',4,4,4);
-INSERT INTO market.products VALUES
-    (5, 'Paperbag Waist Tie Slim Fit Pants',5,5,5);
+INSERT INTO products VALUES
+    (1, 'Shoulder Knot Leopard Print Dress',50,1,1,1);
+INSERT INTO products VALUES
+    (2, 'Buckle Belted Rolled Hem PU Shorts',47.50,2,2,2);
+INSERT INTO products VALUES
+    (3, 'Ruffle Trim Striped Top And High Split Side Pants Set',85.90,3,3,3);
+INSERT INTO products VALUES
+    (4, 'Crisscross Open Back Dot Strappy Romper',120.20,4,4,4);
+INSERT INTO products VALUES
+    (5, 'Paperbag Waist Tie Slim Fit Pants',130.50,5,5,5);
 
 --stocks
-INSERT INTO market.stocks VALUES
+INSERT INTO stocks VALUES
     (1,1,5,1);
-INSERT INTO market.stocks VALUES
+INSERT INTO stocks VALUES
     (2,2,14,2);
-INSERT INTO market.stocks VALUES
+INSERT INTO stocks VALUES
     (3,3,7,3);
-INSERT INTO market.stocks VALUES
+INSERT INTO stocks VALUES
     (4,4,6,4);
-INSERT INTO market.stocks VALUES
+INSERT INTO stocks VALUES
     (5,5,12,5);
--- tables.sql
+++ tables.sql
@@ -1,84 +1,57 @@
-DROP TABLE market.stocks;
-DROP TABLE market.products;
-DROP TABLE market.colors;
-DROP TABLE market.fabric;
-DROP TABLE market.locations;
-DROP TABLE market.product_size;
+SET search_path = market, public;
 
-CREATE TABLE market.colors
+DROP TABLE stocks;
+DROP TABLE products;
+DROP TABLE colors;
+DROP TABLE fabric;
+DROP TABLE locations;
+DROP TABLE product_size;
+
+DROP TABLE IF EXISTS colors CASCADE;
+CREATE TABLE colors
 (
-    id SERIAL,
-    color_id integer NOT NULL,
-    color_name character varying(40) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT colors_pkey PRIMARY KEY (color_id),
-    CONSTRAINT unq_color_name UNIQUE (color_name)
+    color_id SERIAL PRIMARY KEY NOT NULL,
+    color_name VARCHAR(45) UNIQUE NOT NULL
 );
 
-
-CREATE TABLE market.fabric
+DROP TABLE IF EXISTS fabric CASCADE;
+CREATE TABLE fabric
 (
-    id SERIAL,
-    fabric_id integer NOT NULL,
-    fabric_name character varying(40) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT fabric_pkey PRIMARY KEY (fabric_id),
-    CONSTRAINT unq_fabric_name UNIQUE (fabric_name)
+    fabric_id SERIAL PRIMARY KEY NOT NULL,
+    fabric_name VARCHAR(45) UNIQUE NOT NULL
 );
 
-
-CREATE TABLE market.locations
+DROP TABLE IF EXISTS locations CASCADE;
+CREATE TABLE locations
 (
-    id SERIAL,
-    location_id integer NOT NULL,
-    location_name character varying(80) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT locations_pkey PRIMARY KEY (location_id)
+    location_id SERIAL PRIMARY KEY NOT NULL,
+    location_name VARCHAR(45) UNIQUE NOT NULL
 );
 
-CREATE TABLE market.product_size
+DROP TABLE IF EXISTS product_size CASCADE;
+CREATE TABLE product_size
 (
-    id SERIAL,
-    size_id integer NOT NULL,
-    size_name character varying(40) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT pk_size_id PRIMARY KEY (size_id)
+    size_id SERIAL PRIMARY KEY NOT NULL,
+    size_name VARCHAR(45) UNIQUE NOT NULL
 );
 
-CREATE TABLE market.products
+DROP TABLE IF EXISTS products CASCADE;
+CREATE TABLE products
 (
-    id SERIAL,
-    product_id integer NOT NULL,
-    name character varying COLLATE pg_catalog."default" NOT NULL,
-    size_id integer NOT NULL,
-    color_id integer NOT NULL,
-    fabric_id integer NOT NULL,
-    image_url text COLLATE pg_catalog."default",
-    CONSTRAINT pk_product_id PRIMARY KEY (product_id),
-    CONSTRAINT fk_color_id FOREIGN KEY (color_id)
-        REFERENCES market.colors (color_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT fk_fabric_id FOREIGN KEY (fabric_id)
-        REFERENCES market.fabric (fabric_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT fk_size_id FOREIGN KEY (size_id)
-        REFERENCES market.product_size (size_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+    product_id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    price money NOT NULL,
+    size_id INT REFERENCES product_size (size_id) NOT NULL,
+    color_id INT REFERENCES colors (color_id) NOT NULL,
+    fabric_id INT REFERENCES fabric (fabric_id) NOT NULL,
+    image bytea 
 );
 
-CREATE TABLE market.stocks
+DROP TABLE IF EXISTS stocks CASCADE;
+CREATE TABLE stocks
 (
-    id SERIAL,
-    stocks_id integer NOT NULL,
-    product_id integer NOT NULL,
-    amount integer NOT NULL,
-    location_id integer NOT NULL,
-    CONSTRAINT stocks_pkey PRIMARY KEY (id),
-    CONSTRAINT fk_location_id FOREIGN KEY (location_id)
-        REFERENCES market.locations (location_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT fk_product_id FOREIGN KEY (product_id)
-        REFERENCES market.products (product_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+    stocks_id SERIAL PRIMARY KEY NOT NULL,
+    product_id INT REFERENCES products (product_id) NOT NULL,
+    amount INT NOT NULL,
+    location_id INT REFERENCES locations (location_id) NOT NULL
 );
